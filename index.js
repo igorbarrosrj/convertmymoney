6f73e407b5455ed6be8dca5ea069e7d6ba73760b
Igor Barros <igorbarrosrj@gmail.com>
2019-12-30 16:08:13 -0300

    Integração com Banco central

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 
 const convert = require('./lib/convert')
+const bcb = require('./lib/bcb')
 
 
 const port = process.env.PORT || 3000
@@ -11,8 +12,12 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views') )
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res)=>{
-    res.render('home')
+app.get('/', async(req, res)=>{
+    const cotacao = await bcb.getCotacao()
+        res.render('home', { 
+            cotacao
+
+        })
 })
 app.get('/cotacao', (req, res)=>{
     const {cotacao, quantidade} = req.query
